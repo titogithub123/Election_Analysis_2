b9775e0a0ecde5732d87212b6c917131af3fa3b9
titogithub123 <93171738+titogithub123@users.noreply.github.com>
2022-10-10 07:57:29 -0700

    firs commit

---
+++ Python_practice.py
@@ -0,0 +1,12 @@
+counties = ["Arapahoe", "Denver", "Jefferson"]
+
+if counties[3] != 'Jefferson':
+
+    print(counties[2])
+
+    counties
+    if "El Paso" in counties:
+        print("El Paso in counties")
+
+    else:
+        print("El Paso not in counties")
